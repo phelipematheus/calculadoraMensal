44a15a75d8d7b172bb831b0c7db6f960163b5240
Phelipe <phelipe@MacBook-Pro-de-Phelipe.local>
2018-04-09 21:25:54 -0300

    atualizacao ja usando jsf

--- src/main/java/br/com/calculadoraMensal/model/Principal.java
+++ src/main/java/br/com/calculadoraMensal/model/Principal.java
@@ -1,6 +1,14 @@
 package br.com.calculadoraMensal.model;
 
+import javax.faces.bean.ManagedBean;
 
+@ManagedBean
 public class Principal {
 
+	private String mensagem = "Teste de resposta da classe";
+	
+	
+	public String getMensagem() {
+		return mensagem;
+	}
 }
